2c5305dad7b4f32a512725ee23706c77bfcfca3d
jkamz <jkamz6432@gmail.com>
2019-05-26 03:32:10 +0300

    add jquery to manipulate DOM

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ function rootReducer(state = initialState, action) {
 
 //Create Store and pass in root reducer
 const store = Redux.createStore(rootReducer);
+
+//Manipulate DOM using jquery
+$(document).ready(function() {
+  $("#increment").on("click", function() {
+    //dispatch an action to increment count
+  });
+  $("#decrement").on("click", function() {
+    //dispatch an action to decrement count
+  });
+});
